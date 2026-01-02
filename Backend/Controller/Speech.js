30b71a121495dea45ca9f38ebecf642029fcc19a
PrashantGoyal20 <prashant.goyal2002@gmail.com>
2026-01-02 19:19:31 +0530

    stt error fixed

--- Backend/Controller/Speech.js
+++ Backend/Controller/Speech.js
@@ -85,7 +85,7 @@ export const speechAnalyzer = async (req, res, next) => {
 
     await session.save();
 
-    res.status(200).json({ audio: audioBase64 })
+    res.status(200).json({ success:true,audio: audioBase64 })
 
   } catch (error) {
     console.log('STT error:', error);
